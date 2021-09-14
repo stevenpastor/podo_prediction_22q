f75cbb9986b111326d67e567180c5d079d710bf0
stevenpastor <stevenjpastor@gmail.com>
2021-09-14 10:39:08 -0400

    initialized project structure

---
+++ podo_prediction_22q/logistic_regression/logistic_regression.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+"""
+Documentation.
+"""
+
+
+# Built-in imports.
+#import 
+
+# Third-party imports.
+#import
+
+
+def something():
+    """
+    """
+    return
+
+def main():
+    """
+    """
+    return
+
+
+if __name__ == "__main__":
+    main()
